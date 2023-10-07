7b099174b36771df34c8b56e4f8bd0c1331ada21
pexcn <pexcn97@gmail.com>
2023-10-07 16:35:33 +0800

    fix result value start with .

--- daily-oracle-csv.sh
+++ daily-oracle-csv.sh
@@ -32,6 +32,34 @@ _ceil() {
   awk 'function ceil(x, y){y=int(x); return(x>y?y+1:y)} {print ceil($0)}'
 }
 
+_is_start_with() {
+  local str="$1"
+  local sub="$2"
+  [[ $str == ${sub}* ]]
+}
+
+_to_csv() {
+  sed ':a;N;$!ba;s/\n/,/g'
+}
+
+_get_default_iface() {
+  ip route get 8.8.8.8 | sed 's/^.*src \([^ ]*\).*$/\1/;q'
+}
+
+_get_log_dir() {
+  local cur_dir=$(dirname "$0")
+  local sub_dir=$(basename "$0" .sh)
+  echo "${cur_dir}/${sub_dir}_logs"
+}
+
+_get_log_file() {
+  local p1=$(basename "$0" .sh)
+  local p2=$(_get_default_iface | sed 's/\./-/g')
+  local p3=${LOGIN_INFO##*/}
+  local p4=$(date +%Y-%m)
+  echo "${p1}_${p2}_${p3}_${p4}.csv"
+}
+
 get_all_sessions() {
   _exec_sql "SELECT COUNT(1) FROM V\$SESSION;"
 }
@@ -62,38 +90,20 @@ get_tablespace_maximum_size() {
 }
 
 get_tablespace_total_size() {
-  #_exec_sql "SELECT ROUND(SUM(SUM(BYTES))/1024/1024/1024)||' GB' FROM DBA_DATA_FILES GROUP BY TABLESPACE_NAME HAVING TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');"
-  _exec_sql "SELECT ROUND(SUM(BYTES)/1024/1024/1024)||' GB' FROM DBA_DATA_FILES WHERE TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');"
+  #_exec_sql "SELECT ROUND(SUM(SUM(BYTES))/1024/1024/1024,2)||' GB' FROM DBA_DATA_FILES GROUP BY TABLESPACE_NAME HAVING TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');"
+  local value=$(_exec_sql "SELECT ROUND(SUM(BYTES)/1024/1024/1024,2)||' GB' FROM DBA_DATA_FILES WHERE TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');")
+  ! _is_start_with "$value" . || value=${value//./0.}
+  echo "$value"
 }
 
 #get_tablespace_free_size() {
-#  _exec_sql "SELECT ROUND(SUM(SUM(BYTES))/1024/1024/1024)||' GB' FROM DBA_FREE_SPACE GROUP BY TABLESPACE_NAME HAVING TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');"
+#  _exec_sql "SELECT ROUND(SUM(SUM(BYTES))/1024/1024/1024,2)||' GB' FROM DBA_FREE_SPACE GROUP BY TABLESPACE_NAME HAVING TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');"
 #}
 
 get_tablespace_used_percentage() {
-  _exec_sql "SELECT ROUND((SUM(BYTES)/SUM(MAXBYTES))*100,2)||'%' FROM DBA_DATA_FILES WHERE TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');"
-}
-
-_to_csv() {
-  sed ':a;N;$!ba;s/\n/,/g'
-}
-
-_get_default_iface() {
-  ip route get 8.8.8.8 | sed 's/^.*src \([^ ]*\).*$/\1/;q'
-}
-
-_get_log_dir() {
-  local cur_dir=$(dirname "$0")
-  local sub_dir=$(basename "$0" .sh)
-  echo "${cur_dir}/${sub_dir}_logs"
-}
-
-_get_log_file() {
-  local p1=$(basename "$0" .sh)
-  local p2=$(_get_default_iface | sed 's/\./-/g')
-  local p3=${LOGIN_INFO##*/}
-  local p4=$(date +%Y-%m)
-  echo "${p1}_${p2}_${p3}_${p4}.csv"
+  local value=$(_exec_sql "SELECT ROUND((SUM(BYTES)/SUM(MAXBYTES))*100,2)||'%' FROM DBA_DATA_FILES WHERE TABLESPACE_NAME NOT IN ('SYSTEM','SYSAUX','UNDOTBS1');")
+  ! _is_start_with "$value" . || value=${value//./0.}
+  echo "$value"
 }
 
 combine_output() {
