447f181a062c2986a8fcaaed7fe641d4436ab0ec
pexcn <pexcn97@gmail.com>
2024-02-22 11:43:17 +0800

    add server-checker.sh

---
+++ server-checker.sh
@@ -0,0 +1,236 @@
+#!/bin/sh
+#
+# @usage: `./server-checker.sh --help`
+# @author: Sing Yu Chan
+# @version: `./server-checker.sh --version`
+#
+# shellcheck disable=SC2155
+
+PROG_NAME="${0##*/}"
+PROG_VER=20240222
+
+_get_time() {
+  date '+%Y-%m-%d %T'
+}
+
+debug() {
+  [ "$VERBOSE" = 1 ] || return 0
+  local time="$(_get_time)"
+  printf "[${time}] [DEBUG]: %s\n" "$*"
+}
+
+info() {
+  local green='\e[0;32m'
+  local clear='\e[0m'
+  local time="$(_get_time)"
+  printf "${green}[${time}] [INFO]: ${clear}%s\n" "$*"
+}
+
+warn() {
+  local yellow='\e[1;33m'
+  local clear='\e[0m'
+  local time="$(_get_time)"
+  printf "${yellow}[${time}] [WARN]: ${clear}%s\n" "$*" >&2
+}
+
+error() {
+  local red='\e[0;31m'
+  local clear='\e[0m'
+  local time="$(_get_time)"
+  printf "${red}[${time}] [ERROR]: ${clear}%s\n" "$*" >&2
+}
+
+execute() {
+  local login="${1%@*}"
+  local username="${login%%:*}"
+  local password="${login#*:}"
+  local host="${1##*@}"
+  local args="$*"
+  local cmd="${args#* }"
+  debug "$cmd"
+  # ref: https://stackoverflow.com/questions/37066540/bash-while-loop-is-not-looping
+  sshpass -p "$password" ssh -n -q -o "StrictHostKeyChecking=no" -o "ConnectTimeout=5" "$username"@"$host" "$cmd"
+}
+
+_print_version() {
+  local green='\e[0;32m'
+  local clear='\e[0m'
+  printf "${green}%s${clear}\n" "$PROG_VER"
+}
+
+_print_usage() {
+  cat <<EOF
+$PROG_NAME $PROG_VER
+server checker.
+
+USAGE:
+    $PROG_NAME [OPTIONS]
+
+OPTIONS:
+    -t, --target <TARGET_FILE>    Targets and credential information.
+    -o, --output <OUTPUT_FILE>    Path to output csv file.
+    -v, --verbose                 Verbose logging.
+    -V, --version                 Show version.
+    -h, --help                    Show this help message then exit.
+EOF
+}
+
+parse_args() {
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+      -t | --target)
+        TARGET_FILE="$2"
+        shift 2
+        ;;
+      -o | --output)
+        OUTPUT_FILE="$2"
+        shift 2
+        ;;
+      -v | --verbose)
+        VERBOSE=1
+        shift 1
+        ;;
+      -V | --version)
+        _print_version
+        exit 0
+        ;;
+      -h | --help)
+        _print_usage
+        exit 0
+        ;;
+      *)
+        error "unknown option: $1"
+        exit 2
+        ;;
+    esac
+  done
+
+  # parameters checking
+  command -v sshpass >/dev/null || {
+    error "$PROG_NAME requires sshpass, but \`sshpass\` command not found."
+    exit 1
+  }
+  if [ -z "$TARGET_FILE" ]; then
+    error "\`-t | --target\` parameter must be specified."
+    exit 1
+  fi
+  if [ -n "$OUTPUT_FILE" ]; then
+    local output_dir="$(dirname "$OUTPUT_FILE")"
+    [ -d "$output_dir" ] || {
+      warn "output directory does not exist, created it automatically."
+      mkdir -p "$output_dir"
+    }
+    [ ! -f "$OUTPUT_FILE" ] || warn "output file existed, csv will be override to it."
+  fi
+}
+
+CSV_HEADER="ip_address,cpu_usage,mem_total,mem_usage,mem_avail,mem_usage_pct,disk_total,disk_usage,disk_avail,disk_usage_pct"
+
+_byte2gb() {
+  awk '{printf "%.1f\n", $1/1024/1024/1024}'
+}
+
+_kb2gb() {
+  awk '{printf "%.1f\n", $1/1024/1024}'
+}
+
+_ceil() {
+  awk 'function ceil(x, y){y=int(x); return(x>y?y+1:y)} {print ceil($0)}'
+}
+
+_to_csv() {
+  sed ':a;N;$!ba;s/\n/,/g'
+}
+
+_check_cpu_usage() {
+  local token="$1"
+  local cmd="awk '{u=\$2+\$4; t=\$2+\$4+\$5; if (NR==1){u1=u; t1=t;} else printf \"%.1f\\n\", (\$2+\$4-u1) * 100 / (t-t1); }' <(grep 'cpu ' /proc/stat) <(sleep 1; grep 'cpu ' /proc/stat)"
+  execute "$token" "$cmd"
+}
+
+_check_mem_total() {
+  local token="$1"
+  local mem_total_cmd="awk '/MemTotal/ { print \$2*1024 }' /proc/meminfo"
+  execute "$token" "$mem_total_cmd"
+}
+
+_check_mem_avail() {
+  local token="$1"
+  local mem_avail_cmd="awk '/MemAvailable/ { print \$2*1024 }' /proc/meminfo"
+  execute "$token" "$mem_avail_cmd"
+}
+
+_check_mem_usage() {
+  local token="$1"
+  local mem_total="$(_check_mem_total "$token")"
+  local mem_avail="$(_check_mem_avail "$token")"
+  echo $((mem_total - mem_avail))
+}
+
+_check_mem_usage_pct() {
+  local token="$1"
+  local mem_total="$(_check_mem_total "$token")"
+  local mem_usage="$(_check_mem_usage "$token")"
+  echo $((mem_usage * 100 / mem_total))%
+}
+
+_get_disk_info() {
+  local token="$1"
+  # see also the `-P | --portability` option
+  local cmd="df -l -t vfat -t xfs -t ext4 --output=size,used,avail,pcent --total | tail -1"
+  execute "$token" "$cmd"
+}
+
+_check_disk_total() {
+  local token="$1"
+  _get_disk_info "$token" | awk '{print $1}'
+}
+
+_check_disk_usage() {
+  local token="$1"
+  _get_disk_info "$token" | awk '{print $2}'
+}
+
+_check_disk_avail() {
+  local token="$1"
+  _get_disk_info "$token" | awk '{print $3}'
+}
+
+_check_disk_usage_pct() {
+  local token="$1"
+  _get_disk_info "$token" | awk '{print $4}'
+}
+
+_check_server() {
+  local token="$1"
+  local ip_address="${token##*@}"
+  echo "$ip_address"
+  _check_cpu_usage "$token"
+  _check_mem_total "$token" | _byte2gb | _ceil
+  _check_mem_usage "$token" | _byte2gb
+  _check_mem_avail "$token" | _byte2gb
+  _check_mem_usage_pct "$token"
+  _check_disk_total "$token" | _kb2gb | _ceil
+  _check_disk_usage "$token" | _kb2gb
+  _check_disk_avail "$token" | _kb2gb
+  _check_disk_usage_pct "$token"
+}
+
+server_check() {
+  [ -z "$OUTPUT_FILE" ] || echo "$CSV_HEADER" >"$OUTPUT_FILE"
+  while read -r target; do
+    # ignore empty lines
+    [ -n "$target" ] || continue
+    # ignore comment lines
+    [ -n "${target%%#*}" ] || continue
+    # output to csv
+    if [ -n "$OUTPUT_FILE" ]; then
+      _check_server "$target" | _to_csv | tee -a "$OUTPUT_FILE"
+    else
+      _check_server "$target" | _to_csv
+    fi
+  done <"$TARGET_FILE"
+}
+
+parse_args "$@"
+server_check
